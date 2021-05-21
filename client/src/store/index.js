28766f9fc53c5a8a0a110e4756a96ff44b972eaf
조웅현 <tofan@naver.com>
2021-05-21 14:40:33 +0900

    TMDb

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 로그인 확인 변수
     isLogin: false,
+    // Server URL 변수
+    SERVER_URL: process.env.VUE_APP_SERVER_URL,
   },
   mutations: {
   },
--- client/src/views/FrontPage.vue
+++ client/src/views/FrontPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <h1>메인입니다.</h1>
+    <!-- card 형태로  영화 목록을 출력한다. -->
   </div>
 </template>
 
@@ -8,20 +9,23 @@
 import axios from 'axios'
 
 export default {
-  name: 'main',
+  name: 'FrontPage',
   data: function () {
     return {
       movieList: [],
     }
   },
   // 로드될 때 영화 목록을 불러온다.
+  
   created: function () {
     axios({
-      url: '',
-      method: 'POST',
+      url: `${this.$store.state.SERVER_URL}/movies/`,
+      method: 'GET',
     })
     .then(resp => {
-      console.log(resp)
+      console.log(`${this.$store.state.SERVER_URL}`)
+      console.log(resp.data)
+      this.movieList = resp.data
     })
     .catch(err => {
       console.log(err)
