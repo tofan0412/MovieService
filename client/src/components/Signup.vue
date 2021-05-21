278c6b08918d2875f464b2350ce6a8b6fdb897db
조웅현 <tofan@naver.com>
2021-05-21 09:23:43 +0900

    update login axios func

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="signup">
     <label for="username">사용자 아이디</label>
-    <input type="text" name="username" id="username">
+    <input type="text" name="username" id="username" v-model="credentials.username">
     <br>
     <label for="password">비밀번호</label>
-    <input type="text" name="password" id="password">
+    <input type="text" name="password" id="password" v-model="credentials.password">
+    <label for="password">비밀번호 확인</label>
+    <input type="text" name="passwordConfirmation" id="passwordConfirmation" v-model="credentials.passwordConfirmation">
     <button @click="onSignup">회원가입</button>
   </div>
 </template>
@@ -14,10 +16,29 @@ import axios from 'axios'
 const SERVER_URL = process.env.VUE_APP_SERVER_URL
 
 export default {
+  data: function() {
+    return {
+      credentials: {
+        username: null,
+        password: null,
+        passwordConfirmation: null,
+      }
+    }
+  },
   methods: {
     onSignup: function () {
       console.log(SERVER_URL)
-      axios({})
+      axios({
+        url: `${SERVER_URL}/accounts/signup/`,
+        method: 'POST',
+        data: this.credentials,
+      })
+      .then(resp => {
+        console.log(resp)
+      })
+      .catch(err => {
+        console.log(err)
+      })
     }
   }  
 }
