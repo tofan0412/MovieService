b6bd69741745cd68c4e75aef1d16373ebbd473ec
차성민 <ckpow@naver.com>
2021-05-21 16:44:51 +0900

    community 게시판 url, serializers, 조회 및 수정 삭제 부분 진행 생성은 유저토큰 처리 이후 주석풀어주면 됨

--- server/community/views.py
+++ server/community/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def articlelist_create(request): 
+    if request.method=='GET': 
+        articles = Article.objects.all()
+        serializer= ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    # else:
+    #     serializer = ArticleSerializer(data=request.data)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save(user=request.user)
+    #         return Response(serializer.data)
+
+
+@api_view(['PUT', 'DELETE'])
+def article_update_delete(request, article_pk): 
+    article = get_object_or_404(Article, pk=article_pk)
+    if request.method=='PUT' : 
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True): 
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        article.delete()
+        return Response({'id':article_pk}) # 삭제될 때 삭제된 데이터의 id 값을 보여주도록 함.
+        
+
+### 댓글 생성 및 삭제 파트 추가
--- server/movies/views.py
+++ server/movies/views.py
@@ -3,16 +3,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import MovieSerializer
 from .models import Movie
-# import requests
+
 
 @api_view(['GET'])  #일단 GET방식 테스트
 def index(request):
-    # client_id = "cTouCjNxX9puRsipWcFz"
-    # client_secert = "vSOn9KESoJ"
-    # URL = 'https://openapi.naver.com/v1/search/movie.json?display=100&query=어밴저스&start=1&pubDate="2021"' 
-    # request = urllib.request.Request(URL) 
-    # request.add_header("X-Naver-Client-Id", client_id)
-    # request.add_header("X-Naver-Client-Secret", client_secert)
 
     movies = Movie.objects.all()
     serializers = MovieSerializer(movies, many=True)
