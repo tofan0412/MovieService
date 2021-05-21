4358f241655e535df79f88e3cc18ec33f3da0c15
차성민 <ckpow@naver.com>
2021-05-21 13:41:29 +0900

    complete loaddata test

--- server/movies/models.py
+++ server/movies/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=256)
-    subtitle = models.CharField(max_length=256)
-    image = models.TextField()
     link = models.TextField()
+    image = models.TextField()
+    subtitle = models.CharField(max_length=256)   
     pubDate = models.CharField(max_length=256)
     director = models.CharField(max_length=256)
     actor = models.CharField(max_length=256)
-    userRating = models.IntegerField()
+    userRating = models.FloatField()
 
 
 class Review(models.Model):
