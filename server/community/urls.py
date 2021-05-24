e198ae340d43f30e4b9d3e1e9a875f57d689ec25
차성민 <ckpow@naver.com>
2021-05-24 09:11:55 +0900

    댓글 기능 추가, 토큰관련 데코레이터들 추가

--- server/community/urls.py
+++ server/community/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.articlelist_create),
     path('<int:article_pk>/', views.article_update_delete),
     # 댓글 생성 및 댓글 삭제 추가 예정
+    path('<int:article_pk>/comment_list_create/', views.comment_list_create),
+    path('<int:comment_pk>/comment_delete/', views.comment_delete),
 ]
--- server/movies/views.py
+++ server/movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from .serializers import MovieSerializer, ReviewSerializer
 from .models import Movie, Review
 
@@ -14,15 +16,17 @@ def index(request):
 
 
 @api_view(['GET'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializers = MovieSerializer(movie)
     return Response(serializers.data)
 
 
-## rating 관련 로직
-
 @api_view(['GET', 'POST'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def rank_list_create(request, movie_pk):
     movie = get_object_or_404(Review, id=movie_pk)
     if request.method=='GET':
@@ -37,6 +41,8 @@ def rank_list_create(request, movie_pk):
 
 
 @api_view(['DELETE'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def rank_delete(request, movie_pk):
     review = get_object_or_404(Review, pk=movie_pk)
     review.delete()
@@ -44,16 +50,16 @@ def rank_delete(request, movie_pk):
 
 
 @api_view(['PUT'])
+@authentication_classes([JSONWebTokenAuthentication])
+@permission_classes([IsAuthenticated])
 def rank_update(request, movie_pk):
-    review = get_object_or_404(Review, id=movie_pk)
+    review = get_object_or_404(Review, id=movie_pk)  
     rank = get_object_or_404(Review, user=request.user)
     rank.delete()
     serializer = ReviewSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user, review=review)
         return Response(serializer.data)
-
-
 
 
 # 좋아요 추가 예정
